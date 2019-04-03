d341cc05dc6eb3e7d42f3c05ac6971dafe18b065
Peter Zernia <peter176@gmail.com>
2019-04-03 19:32:14 +0200

    added root to Tranformation model

--- transformations/models.py
+++ transformations/models.py
@@ -6,6 +6,7 @@ class Transformation(models.Model):
     '''
     Describes all of the Tranformations of a single root in their respective lexical categories.
     '''
+    root = models.CharField(max_length=255, null=False, blank=False)
     adjectives = models.ForeignKey(
         Adjective, on_delete=models.CASCADE, null=True, blank=True)
     adverbs = models.ForeignKey(
@@ -14,3 +15,6 @@ class Transformation(models.Model):
         Noun, on_delete=models.CASCADE, null=True, blank=True)
     verbs = models.ForeignKey(
         Verb, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.root
