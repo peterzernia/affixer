4896899aba08e0bf6a84080935034019644b3e96
Peter Zernia <peter176@gmail.com>
2019-04-03 17:09:57 +0200

    added Word model

--- words/models.py
+++ words/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Word(models.Model):
+    '''
+    Describes the words of the English language as basic parts of speech.
+    '''
+    word = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.word
