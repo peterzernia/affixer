d801d10e0c62feb51732055a0d21f72820365fad
Peter Zernia <peter176@gmail.com>
2019-04-03 11:13:13 +0200

    added words to django app

--- affixer/settings.py
+++ affixer/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'words',
+    'words.apps.WordsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
