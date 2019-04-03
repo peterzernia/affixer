508bd41cf4cfe96fa7e01d36ddec4f9ba58128d7
Peter Zernia <peter176@gmail.com>
2019-04-03 16:41:28 +0200

    test

--- affixer/settings.py
+++ affixer/settings.py
@@ -114,9 +114,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'frontend/build/static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Heroku settings get overridden if local_settings.py exists.
